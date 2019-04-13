5b2b893fa9f6c48ccb39de847ccfffef1c163dca
pdoubravova <pdoubravova@profinit.eu>
2019-04-13 20:33:13 +0200

    hotovo

--- advancer1_0/solution.py
+++ advancer1_0/solution.py
@@ -12,11 +12,10 @@ Read all data from dataset.
 
 
 def load_data():
-    # TODO: predelat na relativni cestu
-    book_ratings = pd.read_csv('C:\\Users\\pdoubravova\\Documents\\work\\advancer1_0\\BX-Book-Ratings.csv',
+    book_ratings = pd.read_csv('BX-Book-Ratings.csv',
                                error_bad_lines=False, delimiter=';', encoding='latin-1',
                                dtype={'User-ID': 'category', 'ISBN': 'category', 'Book-Rating': np.int32})
-    book_list = pd.read_csv('C:\\Users\\pdoubravova\\Documents\\work\\advancer1_0\\BX-Books.csv', delimiter=';',
+    book_list = pd.read_csv('BX-Books.csv', delimiter=';',
                             error_bad_lines=False, encoding='latin-1')
     # users = pd.read_csv('C:\\Users\\pdoubravova\\Documents\\work\\advancer1_0\\BX-Users.csv', delimiter = ';', error_bad_lines = False, encoding = 'latin-1', )
     return book_ratings, book_list
@@ -28,30 +27,22 @@ Preprocessing data, check data quality
 
 
 def book_ratings_preprocess(books, list):
-    print(len(books))
     books = books[books['ISBN'].isin(list)]
-    print(len(books))
-    books.loc[:, 'Book-Rating'] = books['Book-Rating'].replace(0, np.NaN)
+    books.loc[:, 'Book-Rating'] = books['Book-Rating'].replace(0, np.NaN)  # TODO je to potřeba?
     books = books[books['Book-Rating'].notnull()].copy()
-    print(len(books))
     # books = books.iloc[:10]
     return books
-
-
-"""
-vypocet
-"""
 
 
 def calculate_mean(books_input):
     # prumer pro kazdeho uzivatele
     books = books_input
     kukykuky = books.groupby('User-ID')['Book-Rating'].transform('mean')
-    books['rating_mean'] = kukykuky
-    books = books.drop_duplicates('User-ID')
+    books['usr_rat_mean'] = kukykuky
+    # books = books.drop_duplicates('User-ID')
 
     # odstranim uzivatele co nic nehodnotili
-    books = books[books['rating_mean'].notnull()].copy()
+    books = books[books['usr_rat_mean'].notnull()].copy()
     return books
 
 
@@ -65,60 +56,56 @@ def get_weights(selected_book, border1, border2, border3):
     else:
         return 0
 
+
+def vypis(data, list, pocet):
+    recommended_isbns = data.head(pocet)
+    book_names = list[list['ISBN'].isin(recommended_isbns.index)]
+    book_names_only = book_names.loc[:, ['Book-Title', 'Book-Author']]
+    print("I recommend you these books:")
+    print(book_names_only)
+
+
 def get_groups(data, percentile90, percentile80, percentile70):
     data = data.reset_index(drop=True)
-    print(len(percentile_80))
+    weights = []
+    print(len(percentile80))
     for index, row in data.iterrows():
-        print(index)
-
         weight = get_weights(row['Book-Rating'], percentile90[index], percentile80[index], percentile70[index])
-
-################################################################
-
-
-book_ratings, book_list = load_data()
-book_ratings = book_ratings_preprocess(book_ratings, book_list['ISBN'])
-RATINGS = book_ratings
-book_ratings = calculate_mean(book_ratings)
-
-# find user with tolkien above mean
-tolkien = '0345339703'
-
-selected_users = RATINGS[RATINGS['ISBN'] == tolkien]
-
-selected_users_above = selected_users[selected_users['Book-Rating'] / selected_users['rating_mean'] > 1]
-
-selected_users_above_list = selected_users_above['User-ID']
+        weights.append(weight)
+    return pd.Series(data=weights, index=data['User-ID'])
 
 
-# ziskam matici: uzivatele a kniha
-selected_data = RATINGS[RATINGS['User-ID'].isin(selected_users_above_list)]
+def main():
+    book_ratings, book_list = load_data()
+    book_ratings = book_ratings_preprocess(book_ratings, book_list['ISBN'])
+    book_ratings = calculate_mean(book_ratings)  # TODO tady se to meni na unique user, to nechceme
 
-selection_matrix = selected_data.pivot(index='User-ID', columns='ISBN', values='Book-Rating')
+    # find user with tolkien above mean
+    tolkien = '0345339703'  # TODO je to nerozumnejsi kniha?
 
-percentile_90 = selection_matrix.apply(lambda x: np.nanpercentile(x, 90), axis=1)
-percentile_80 = selection_matrix.apply(lambda x: np.nanpercentile(x, 80), axis=1)
-percentile_70 = selection_matrix.apply(lambda x: np.nanpercentile(x, 70), axis=1)
+    selected_tolkien_only = book_ratings[book_ratings['ISBN'] == tolkien]
+
+    # selected_users_above = selected_tolkien_only[selected_tolkien_only['Book-Rating'] / selected_tolkien_only['usr_rat_mean'] > 1]
+    tolkien_users_list = selected_tolkien_only['User-ID']
+
+    # ziskam matici: uzivatele a kniha
+    tolkien_usrs_ratings = book_ratings[book_ratings['User-ID'].isin(tolkien_users_list)]  # TODO tohle je nějak špatně
+    selection_matrix = tolkien_usrs_ratings.pivot(index='User-ID', columns='ISBN', values='Book-Rating')
+
+    percentile_90 = selection_matrix.apply(lambda x: np.nanpercentile(x, 90), axis=1)
+    percentile_80 = selection_matrix.apply(lambda x: np.nanpercentile(x, 80), axis=1)
+    percentile_70 = selection_matrix.apply(lambda x: np.nanpercentile(x, 70), axis=1)
+
+    weights = get_groups(selected_tolkien_only, percentile_90, percentile_80, percentile_70)
+    selection_matrix = selection_matrix[selection_matrix.index.isin(weights[weights != 0].index)]
+
+    weights = weights[weights!=0]
+    book_means = selection_matrix.mul(weights, axis=0)
+    book_means = book_means.sum().sort_values(ascending=False)
+
+    # fancy vypis
+    vypis(book_means, book_list, 40)
 
 
-get_groups(selected_users, percentile_90, percentile_80, percentile_70)
-#TODO get user in groups
-
-
-selected_data = selected_data[selected_data['ISBN'] != tolkien]
-grouped_data = selected_data[['ISBN']]
-grouped_data['mean_rating'] = selected_data.groupby(['ISBN'])['Book-Rating'].transform("mean")
-# grouped_data['10th_percentile'] = selected_data['']
-grouped_data.drop_duplicates('ISBN')
-grouped_data = grouped_data.sort_values('mean_rating', ascending=False)
-
-print("Finished.")
-print(selection_matrix)
-
-# fancy vypis
-recommended_isbns = grouped_data.head(10)['ISBN']
-book_names = book_list[book_list['ISBN'].isin(recommended_isbns)]
-book_names_only = book_names.loc[:, ['Book-Title', 'Book-Author']]
-print("I recommend you these books:")
-
-print(book_names_only)
+if __name__ == "__main__":
+    main()
